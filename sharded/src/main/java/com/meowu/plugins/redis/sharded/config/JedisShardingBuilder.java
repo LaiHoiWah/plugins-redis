bf6f073bbfbcbb5d5ab0b0f58fc6af324efbb94f
lihaihua <zhangbo>
2022-06-17 13:44:17 +0800

    初始化项目

---
+++ sharded/src/main/java/com/meowu/plugins/redis/sharded/config/JedisShardingBuilder.java
@@ -0,0 +1,40 @@
+package com.meowu.plugins.redis.sharded.config;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
+import redis.clients.jedis.DefaultJedisClientConfig;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisClientConfig;
+import redis.clients.jedis.JedisSharding;
+
+import java.util.List;
+
+public class JedisShardingBuilder{
+
+    private List<String> shardsString;
+
+    private List<HostAndPort> shards;
+    private JedisClientConfig config;
+
+    public JedisShardingBuilder(){
+
+    }
+
+    public JedisSharding builder(){
+        if(CollectionUtils.isEmpty(shards)){
+            defaultShards();
+        }
+
+        return new JedisSharding(this.shards, this.config);
+    }
+
+    private void defaultShards(){
+        shards = Lists.newArrayList(new HostAndPort("localhost", 6379));
+    }
+
+    private void defaultConfig(){
+        config = DefaultJedisClientConfig.builder()
+                                         .
+                                         .build();
+    }
+}
